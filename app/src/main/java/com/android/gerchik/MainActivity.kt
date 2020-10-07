dfe67435ed286b4c9a39e84a2c5fb3f20f9614ed
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-10-07 21:25:23 +0300

    Delete anko dependencies

--- app/src/main/java/com/android/gerchik/MainActivity.kt
+++ app/src/main/java/com/android/gerchik/MainActivity.kt
@@ -3,7 +3,9 @@ package com.android.gerchik
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Just for running application
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
